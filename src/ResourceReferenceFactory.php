57577ac9f3f32d7d5189d9e4c024b918334506b2
Jon Cram <webignition@gmail.com>
2023-04-12 09:20:53 +0100

    Type hint as ResourceReferenceInterface (#94)

--- src/ResourceReferenceFactory.php
+++ src/ResourceReferenceFactory.php
@@ -6,10 +6,11 @@ namespace SmartAssert\ResultsClient;
 
 use SmartAssert\ArrayInspector\ArrayInspector;
 use SmartAssert\ResultsClient\Model\ResourceReference;
+use SmartAssert\ResultsClient\Model\ResourceReferenceInterface;
 
 class ResourceReferenceFactory
 {
-    public function create(ArrayInspector $data): ?ResourceReference
+    public function create(ArrayInspector $data): ?ResourceReferenceInterface
     {
         $label = $data->getNonEmptyString('label');
         $reference = $data->getNonEmptyString('reference');
