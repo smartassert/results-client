f275f4343004d820cfb78fcbf84a8465c0a9a8a4
Jon Cram <webignition@gmail.com>
2023-06-12 15:36:04 +0100

    Add Job::state

--- tests/Integration/CreateJobTest.php
+++ tests/Integration/CreateJobTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace SmartAssert\ResultsClient\Tests\Integration;
 
 use SmartAssert\ResultsClient\Model\Job;
+use SmartAssert\ResultsClient\Model\JobState;
 use Symfony\Component\Uid\Ulid;
 
 class CreateJobTest extends AbstractIntegrationTest
@@ -16,5 +17,8 @@ class CreateJobTest extends AbstractIntegrationTest
 
         $job = self::$client->createJob(self::$user1ApiToken->token, $jobLabel);
         self::assertInstanceOf(Job::class, $job);
+
+        self::assertSame($jobLabel, $job->label);
+        self::assertEquals(new JobState('awaiting-events', null), $job->state);
     }
 }
