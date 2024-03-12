0ae7546b43284f124a7f41fa0b539c7b678eb529
Jon Cram <webignition@gmail.com>
2024-03-12 16:32:24 +0000

    Revert "Create JobInterface (#126)" (#130)
    This reverts commit 34b4db8e2ac3ddf312fae98f912c45f00793ae5f.

--- src/Model/Job.php
+++ src/Model/Job.php
@@ -4,31 +4,16 @@ declare(strict_types=1);
 
 namespace SmartAssert\ResultsClient\Model;
 
-readonly class Job implements JobInterface
+readonly class Job
 {
     /**
      * @param non-empty-string $label
      * @param non-empty-string $token
      */
     public function __construct(
-        private string $label,
-        private string $token,
-        private JobState $state,
+        public string $label,
+        public string $token,
+        public JobState $state,
     ) {
-    }
-
-    public function getLabel(): string
-    {
-        return $this->label;
-    }
-
-    public function getToken(): string
-    {
-        return $this->token;
-    }
-
-    public function getState(): JobState
-    {
-        return $this->state;
     }
 }
