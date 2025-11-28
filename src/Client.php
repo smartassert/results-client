c118233ad17e390f99ba04b4f0b1f2ae48d904ec
Jon Cram <webignition@gmail.com>
2025-11-28 11:13:47 +0000

    Create ClientInterface (#154)

--- src/Client.php
+++ src/Client.php
@@ -16,9 +16,7 @@ use SmartAssert\ResultsClient\Model\JobState;
 use SmartAssert\ServiceClient\Authentication\BearerAuthentication;
 use SmartAssert\ServiceClient\Client as ServiceClient;
 use SmartAssert\ServiceClient\Exception\CurlExceptionInterface;
-use SmartAssert\ServiceClient\Exception\HttpResponseExceptionInterface;
 use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
-use SmartAssert\ServiceClient\Exception\InvalidResponseDataException;
 use SmartAssert\ServiceClient\Exception\InvalidResponseTypeException;
 use SmartAssert\ServiceClient\Exception\NonSuccessResponseException;
 use SmartAssert\ServiceClient\Exception\UnauthorizedException;
@@ -26,7 +24,7 @@ use SmartAssert\ServiceClient\Payload\JsonPayload;
 use SmartAssert\ServiceClient\Request;
 use SmartAssert\ServiceClient\Response\JsonResponse;
 
-readonly class Client
+readonly class Client implements ClientInterface
 {
     public function __construct(
         private string $baseUrl,
@@ -35,19 +33,6 @@ readonly class Client
     ) {
     }
 
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $label
-     *
-     * @throws ClientExceptionInterface
-     * @throws NetworkExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws InvalidResponseDataException
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
     public function createJob(string $token, string $label): Job
     {
         $response = $this->makeJobRequest('POST', $token, $label);
@@ -67,19 +52,6 @@ readonly class Client
         return new Job($label, $token, new JobState($state, $endState));
     }
 
-    /**
-     * @param non-empty-string $token
-     * @param non-empty-string $label
-     *
-     * @throws ClientExceptionInterface
-     * @throws NetworkExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws InvalidResponseDataException
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
     public function getJobStatus(string $token, string $label): JobState
     {
         $response = $this->makeJobRequest('GET', $token, $label);
@@ -96,17 +68,6 @@ readonly class Client
         return new JobState($state, $endState);
     }
 
-    /**
-     * @throws ClientExceptionInterface
-     * @throws NetworkExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws InvalidResponseDataException
-     * @throws InvalidJobTokenException
-     * @throws InvalidModelDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
     public function addEvent(string $jobToken, EventInterface $event): EventInterface
     {
         try {
@@ -134,22 +95,6 @@ readonly class Client
         return $createdEvent;
     }
 
-    /**
-     * @param non-empty-string      $token
-     * @param non-empty-string      $jobLabel
-     * @param null|non-empty-string $reference
-     * @param null|non-empty-string $type
-     *
-     * @return EventInterface[]
-     *
-     * @throws ClientExceptionInterface
-     * @throws NetworkExceptionInterface
-     * @throws HttpResponseExceptionInterface
-     * @throws CurlExceptionInterface
-     * @throws InvalidResponseDataException
-     * @throws InvalidResponseTypeException
-     * @throws UnauthorizedException
-     */
     public function listEvents(string $token, string $jobLabel, ?string $reference, ?string $type): array
     {
         $url = $this->createUrl('/event/list/' . $jobLabel);
