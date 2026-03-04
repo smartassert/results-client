e72da667f4655292fc89f5fa978f76a62e926408
Jon Cram <webignition@gmail.com>
2026-03-04 16:33:10 +0000

    Add method to add event and return only boolean (#168)

--- tests/Functional/Client/AbstractClientModelCreationTestCase.php
+++ tests/Functional/Client/AbstractClientModelCreationTestCase.php
@@ -5,10 +5,31 @@ declare(strict_types=1);
 namespace SmartAssert\ResultsClient\Tests\Functional\Client;
 
 use GuzzleHttp\Psr7\Response;
+use PHPUnit\Framework\Attributes\DataProvider;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Message\ResponseInterface;
+use SmartAssert\ResultsClient\Tests\Functional\DataProvider\InvalidJsonResponseExceptionDataProviderTrait;
 use SmartAssert\ServiceClient\Exception\InvalidModelDataException;
 
 abstract class AbstractClientModelCreationTestCase extends AbstractClientTestCase
 {
+    use InvalidJsonResponseExceptionDataProviderTrait;
+
+    /**
+     * @param class-string<\Throwable> $expectedExceptionClass
+     */
+    #[DataProvider('invalidJsonResponseExceptionDataProvider')]
+    public function testClientActionThrowsResponseException(
+        ClientExceptionInterface|ResponseInterface $httpFixture,
+        string $expectedExceptionClass,
+    ): void {
+        $this->mockHandler->append($httpFixture);
+
+        $this->expectException($expectedExceptionClass);
+
+        ($this->createClientActionCallable())();
+    }
+
     public function testClientActionThrowsInvalidModelDataException(): void
     {
         $responsePayload = ['key' => 'value'];
--- tests/Integration/AddEventTest.php
+++ tests/Integration/AddEventTest.php
@@ -7,7 +7,6 @@ namespace SmartAssert\ResultsClient\Tests\Integration;
 use PHPUnit\Framework\Attributes\DataProvider;
 use SmartAssert\ResultsClient\Exception\InvalidJobTokenException;
 use SmartAssert\ResultsClient\Model\Event;
-use SmartAssert\ResultsClient\Model\EventInterface;
 use SmartAssert\ResultsClient\Model\ResourceReference;
 use SmartAssert\ResultsClient\Model\ResourceReferenceCollection;
 
@@ -30,14 +29,12 @@ class AddEventTest extends AbstractIntegrationTestCase
         );
     }
 
-    /**
-     * @param callable(string): EventInterface $expectedEventCreator
-     */
     #[DataProvider('addSuccessDataProvider')]
-    public function testAddSuccess(Event $event, callable $expectedEventCreator): void
+    public function testAddSuccess(Event $event): void
     {
-        $jobEvent = self::$client->addEvent(self::$user1Job->token, $event);
-        self::assertEquals($expectedEventCreator(self::$user1JobLabel), $jobEvent);
+        self::assertTrue(
+            self::$client->addEvent(self::$user1Job->token, $event),
+        );
     }
 
     /**
@@ -53,16 +50,6 @@ class AddEventTest extends AbstractIntegrationTestCase
                     new ResourceReference('event_label_1', 'event_reference_1'),
                     []
                 ),
-                'expectedEventCreator' => function (string $jobLabel): EventInterface {
-                    \assert('' !== $jobLabel);
-
-                    return (new Event(
-                        1,
-                        'event_type',
-                        new ResourceReference('event_label_1', 'event_reference_1'),
-                        [],
-                    ))->withJob($jobLabel);
-                },
             ],
             'without related references, non-empty body' => [
                 'event' => new Event(
@@ -74,51 +61,21 @@ class AddEventTest extends AbstractIntegrationTestCase
                         'key2' => 'value2',
                     ]
                 ),
-                'expectedEventCreator' => function (string $jobLabel): EventInterface {
-                    \assert('' !== $jobLabel);
-
-                    return (new Event(
-                        2,
-                        'event_type',
-                        new ResourceReference('event_label_2', 'event_reference_2'),
-                        [
-                            'key1' => 'value1',
-                            'key2' => 'value2',
-                        ],
-                    ))->withJob($jobLabel);
-                },
             ],
             'with single related reference, empty body' => [
-                'event' => (new Event(
+                'event' => new Event(
                     3,
                     'event_type',
                     new ResourceReference('event_label_3', 'event_reference_3'),
                     [],
-                ))->withRelatedReferences(
+                )->withRelatedReferences(
                     new ResourceReferenceCollection([
                         new ResourceReference('event_label_1', 'event_reference_1'),
                     ])
                 ),
-                'expectedEventCreator' => function (string $jobLabel): EventInterface {
-                    \assert('' !== $jobLabel);
-
-                    return (new Event(
-                        3,
-                        'event_type',
-                        new ResourceReference('event_label_3', 'event_reference_3'),
-                        [],
-                    ))
-                        ->withJob($jobLabel)
-                        ->withRelatedReferences(
-                            new ResourceReferenceCollection([
-                                new ResourceReference('event_label_1', 'event_reference_1'),
-                            ])
-                        )
-                    ;
-                },
             ],
             'with multiple related references, non-empty body' => [
-                'event' => (new Event(
+                'event' => new Event(
                     4,
                     'event_type',
                     new ResourceReference('event_label_4', 'event_reference_4'),
@@ -126,29 +83,9 @@ class AddEventTest extends AbstractIntegrationTestCase
                         'key3' => 'value3',
                         'key4' => 'value4',
                     ],
-                ))->withRelatedReferences(new ResourceReferenceCollection([
+                )->withRelatedReferences(new ResourceReferenceCollection([
                     new ResourceReference('event_label_1', 'event_reference_1'),
                 ])),
-                'expectedEventCreator' => function (string $jobLabel): EventInterface {
-                    \assert('' !== $jobLabel);
-
-                    return (new Event(
-                        4,
-                        'event_type',
-                        new ResourceReference('event_label_4', 'event_reference_4'),
-                        [
-                            'key3' => 'value3',
-                            'key4' => 'value4',
-                        ],
-                    ))
-                        ->withJob($jobLabel)
-                        ->withRelatedReferences(
-                            new ResourceReferenceCollection([
-                                new ResourceReference('event_label_1', 'event_reference_1'),
-                            ])
-                        )
-                    ;
-                },
             ],
         ];
     }
